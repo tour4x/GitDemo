ebe5335b1ef9235398b4af7934a9adaffe90fcf8
X <x@xdeimac.local>
2018-03-14 10:08:03 +0800

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by X on 2018/3/14.
+//  Copyright © 2018年 leyoubms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
